5cc151aa9b1f4eebdbafa0f48c7ba481d2aa4a57
Noch4nce <m4rgera123@mail.ru>
2022-09-08 00:44:57 +0500

    selector

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { inputText } from './redux/actions'
 
 const Title = (props) => {
 	console.log(props, 'TITLE')
 	const dispatch = useDispatch()
+	const text = useSelector((state) => {
+		const { inputReducer } = state
+		console.log(state, "STATE")
+		return inputReducer.text
+	})
 
 	const handleChange = (event) => {
 		dispatch(inputText(event.target.value))
@@ -15,6 +20,7 @@ const Title = (props) => {
 			<div className="card-title-top">
 				<input type="text" onChange={handleChange} />
 			</div>
+			<p>{text}</p>
 		</div>
 	)
 }
