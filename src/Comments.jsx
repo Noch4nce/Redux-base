2b75aad0b4bfe68138ad2cf209794a7f38db80c7
Noch4nce <m4rgera123@mail.ru>
2022-09-08 01:19:16 +0500

    handle input

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SingleComment from './SingleComment'
 
 const Comments = (props) => {
 	console.log(props, 'COMMNETS')
+	const [textComment, setTextComment] = useState('')
+
+	const handleInput = (e) => {
+		setTextComment(e.target.value)
+	}
+
 	return (
 		<div className="card-comments">
 			<form className="comments-item-create">
-				<input type="text" />
+				<input type="text" value={textComment} onChange={handleInput} />
 				<input type="submit" hidden />
 			</form>
 			<SingleComment />
