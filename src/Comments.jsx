7016fbd03cf4817ecc150e00e098077ec323c805
Noch4nce <m4rgera123@mail.ru>
2022-09-08 00:55:07 +0500

    SingleComment comp

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import SingleComment from './SingleComment'
 
 const Comments = (props) => {
-	console.log(props, "COMMNETS")
+	console.log(props, 'COMMNETS')
 	return (
 		<div className="card-comments">
 			<form className="comments-item-create">
 				<input type="text" />
 				<input type="submit" hidden />
 			</form>
+			<SingleComment />
 		</div>
 	)
 }
